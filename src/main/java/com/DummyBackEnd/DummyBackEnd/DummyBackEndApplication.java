92712139d48c3151c3389fe1cb015804bad3e2d8
Amit <amitsunar54321@gmail.com>
2023-05-03 11:27:26 +0530

    initial commit

---
+++ src/main/java/com/DummyBackEnd/DummyBackEnd/DummyBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.DummyBackEnd.DummyBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DummyBackEndApplication.class, args);
+	}
+}
